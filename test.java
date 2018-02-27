78dffe35193bf40e72b220fea425a4a517db2283
Tommy <tjohnson.2016@sis.smu.edu.sg>
2018-02-27 10:57:02 +0800

    First commit

---
+++ test.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+public class test{
+	public static void main(String[] args){
+		int[] a1 = new int[5];
+		a1[0] = 1;
+		a1[1] = 2;
+		a1[2] = 3;
+		a1[3] = 4;
+		a1[4] = 5;
+		String[] a2 = {"a","b","c","d","e"};
+		
+		for(int i = 0; i < a1.length;i++){
+				
+			System.out.print(a1[i]);
+			System.out.print(a2[i]);
+			
+			System.out.println("");
+		}
+		
+	}
+
+
+
+}
+
